84c238ce5ffcfcd5aca75477b18b335ecc32eb44
Kenyon <hg7959@wayne.edu>
2024-06-27 21:36:09 -0400

    adding problem

---
+++ oe.py
@@ -0,0 +1,12 @@
+def main():
+    ##print("Enter a number")
+
+    num = int(input("Enter a number: "))
+
+    if num % 2 == 0:
+        print(num,"is an even number.")
+    else:
+        print(num,"is an odd number.")
+
+
+main()
